d67318f2038764352fd0827419c8fae31722b921
Valery Sorokin <podrabotka-2019@yandex.ru>
2022-08-08 04:04:30 +0300

    first commit

---
+++ task_1.cpp
@@ -0,0 +1,38 @@
+﻿#define MODE 1
+#ifndef MODE
+#error "Необходимо определить MODE"
+#endif
+
+#include <iostream>
+
+#if MODE == 0
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+    return 0;
+}
+#elif MODE == 1
+
+int add(int a, int b) { return a + b; }
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    std::cout << "Введите число 1:";
+    int a = 0;
+    std::cin >> a;
+    std::cout << "ВВедите число 2: ";
+    int b = 0;
+    std::cin >> b;
+    std::cout << "Результат сложения: " << add(a, b);
+    return 0;
+}
+#else
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Неизвестный режим. Завершение работы." << std::endl;
+}
+
+#endif
